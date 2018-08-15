7e58782f2c259fe6b10a6f11b4161425e9c0dd1e
fabio.fxva <fabio.fxva@gmail.com>
2018-08-15 17:17:38 -0400

    trabalhando no menu

--- header.php
+++ header.php
@@ -7,29 +7,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ThinksCode</title>
     <link rel="stylesheet" href="src/css/fontello.css">
+	<link rel="stylesheet" href="src/css/fontello-codes.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600i,700i" rel="stylesheet">
-    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="dev/css/style.css">
 </head>
 
 <body>
-    <input type="checkbox" id="show" class="show">
-        <nav class="menu-mobile">
-            <ul class="social">
-                <i class="icon icon-facebook"></i>
-                <i class="icon icon-github"></i>
-                <i class="icon icon-mail-alt"></i>
-                <i class="icon icon-phone"></i>
-            </ul>
-            <ul class="menu-item">
-                <li class="item">Home</li>
-                <li class="item">Serviços</li>
-                <li class="item">Sobre</li>
-                <li class="item">Portifólio</li>
-                <li class="item">Contato</li>
-            </ul>
-        </nav>
-        <section class="logo">
-            <a class="link" href="#"><h1 class="text">Logo</h1></a>
-        </section>
+	<header class="header">
+			<input type="checkbox" id="show" class="show">
+					<nav class="menu-mobile menu">
+							<ul class="social">
+									<label class="close" for="show"><i class="icon ">x</i></label>
+									<i class="icon icon-facebook"></i>
+									<i class="icon icon-github"></i>
+									<i class="icon icon-mail-alt"></i>
+									<i class="icon icon-phone"></i>
+							</ul>
+							<ul class="menu-item">
+									<li class="item">Home</li>
+									<li class="item">Serviços</li>
+									<li class="item">Sobre</li>
+									<li class="item">Portifólio</li>
+									<li class="item">Contato</li>
+							</ul>
+					</nav>
+					<div class="logo">
+							<a class="link" href="#"><h1 class="text">Logo</h1></a>
+					</div>
+		</header>
+	<main>
 
            
